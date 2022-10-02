a89c5a82ef3e827dec475b0d913ff5af14afd519
Rholang <rholang@protonmail.com>
2022-10-02 13:25:40 +0200

    bugfix package.json

--- apps/dist/index.js
+++ apps/dist/index.js
@@ -1,17 +1,12 @@
-const fileName_1 =
-  'C:/Users/To/Desktop/Programme/Javascript/Effect-lit/apps/html-4/apps/src/index.ts';
-import * as tsplus_module_1 from '@effect/html/io/Render';
-import * as tsplus_module_2 from '@effect/core/io/Effect/operations/map';
-import * as tsplus_module_3 from '@effect/core/io/Effect/operations/runtime';
+const fileName_1 = "C:/Users/To/Desktop/Programme/Javascript/Effect-lit/apps/html-4/apps/src/index.ts";
+import * as tsplus_module_1 from "@effect/html/io/Render";
+import * as tsplus_module_2 from "@effect/core/io/Effect/operations/map";
+import * as tsplus_module_3 from "@effect/core/io/Effect/operations/runtime";
 //import './globals.ts';
 import './index.css';
-const myPage = tsplus_module_1.html` <div>Here's my main page.</div> `;
-const myListView = tsplus_module_1.html` <input class="border-2 border-rose-500 ..." />`;
-const myPage2 = tsplus_module_1.html` ${myPage} ${myListView} `;
-const program = tsplus_module_2.map_(
-  tsplus_module_1.render(document.body, tsplus_module_1.html` ${myPage2}`),
-  () => void 0,
-  fileName_1 + ':13:4'
-);
-tsplus_module_3.unsafeRunPromise(program, fileName_1 + ':16:25');
+const myPage = tsplus_module_1.html ` <div>Here's my main page3.</div> `;
+const myListView = tsplus_module_1.html ` <input class="border-2 border-rose-600 ..." />`;
+const myPage2 = tsplus_module_1.html ` ${myPage} ${myListView} `;
+const program = tsplus_module_2.map_(tsplus_module_1.render(document.body, tsplus_module_1.html ` ${myPage2}`), () => void 0, fileName_1 + ":13:4");
+tsplus_module_3.unsafeRunPromise(program, fileName_1 + ":16:25");
 //# sourceMappingURL=index.js.map
--- apps/src/index.ts
+++ apps/src/index.ts
@@ -3,7 +3,7 @@ import './index.css';
 //import '@effect/html/global';
 import type {} from '@effect/html/global';
 
-const myPage = html` <div>Here's my main page.</div> `;
+const myPage = html` <div>Here's my main page3.</div> `;
 
 const myListView = html` <input class="border-2 border-rose-600 ..." />`;
 
